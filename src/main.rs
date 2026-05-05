fa76e09db21cd7530776244b65d0352bb503184a
EstanislaoE <edgar1estanislao@gmail.com>
2026-05-04 22:45:17 -0700

    fin

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,50 @@ fn main(){
         }
     };
 
+    if choice == 1 {
+        celsius_to_fahrenheit();
+    } else if choice == 2 {
+        fahrenheit_to_celsius();
+    } else {
+        println!("Invalid choise. Select 1 or 2");
+    }
+
+}
+
+fn celsius_to_fahrenheit(){
+    println!("Enter temperature in Celcius");
+    let mut temp = String::new();
+    io::stdin().read_line(&mut temp).expect("Failed to read input");
+
+    let temp: f64 = match temp.trim().parse(){
+        Ok(num) => num, 
+        Err(_) => {
+            print!("Invlid input. Enter valid number");
+            return;
+        }
+    }; 
+
+    let fahrenheit =  (temp * 9.0/5.0) + 32.0; 
+    print!("{:.2} C is {:.2} F", temp, fahrenheit); 
+
+}
+
+fn fahrenheit_to_celsius(){
+    print!("Enter temperature in Fahrenheit: ");
+
+    let mut temp = String::new();
+    io::stdin().read_line(&mut temp).expect("Failed to read input"); 
+
+    let temp: f64 = match temp.trim().parse(){
+        Ok(num) => num, 
+        Err(_) => {
+            println!("Invliad input. Enter valid number"); 
+            return; 
+        }
+    }; 
+
+    let celcius = (temp - 32.0) * 5.0/9.0;
+    print!("{:.2} F is {:.2} C", temp, celcius); 
 
 }
 
